c00fabd9bd45ef4449c4df13abc589bfc34feeeb
andou1977 <andouemmanuel@gmail.com>
2024-08-05 08:42:56 +0100

    Premier commit

---
+++ src/main/java/com/example/rest/RestApplication.java
@@ -0,0 +1,12 @@
+package com.example.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+    // Configuration supplémentaire si nécessaire
+
+
+
+}
